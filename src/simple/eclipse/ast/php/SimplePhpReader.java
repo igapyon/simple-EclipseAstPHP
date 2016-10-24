2d630adbebc818ec649f9faa56c81a0e03ba7f34
Toshiki Iga <igapyon@gmail.com>
2016-10-24 22:52:29 +0900

    update

--- src/simple/eclipse/ast/php/SimplePhpReader.java
+++ src/simple/eclipse/ast/php/SimplePhpReader.java
@@ -1,29 +1,17 @@
 package simple.eclipse.ast.php;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.File;
 
+import org.apache.commons.io.FileUtils;
 import org.eclipse.php.internal.core.PHPVersion;
 import org.eclipse.php.internal.core.ast.nodes.ASTParser;
 import org.eclipse.php.internal.core.ast.nodes.Program;
 
 public class SimplePhpReader {
 	public static void main(final String[] args) throws Exception {
-		final BufferedReader reader = new BufferedReader(
-				new InputStreamReader(new FileInputStream("./meta/helloworld.txt"), "UTF-8"));
-		final StringBuffer buf = new StringBuffer();
-		for (;;) {
-			String line = reader.readLine();
-			if (line == null)
-				break;
-			buf.append(line);
-			buf.append('\n');
-		}
-		reader.close();
-
 		final ASTParser parser = ASTParser.newParser(PHPVersion.PHP7_1);
-		parser.setSource(buf.toString().toCharArray());
+		parser.setSource(FileUtils.readFileToString(
+			new File("./meta/helloworld.txt"), "UTF-8").toCharArray());
 
 		final Program node = parser.createAST(null);
 
